f32a5fc422b8753cfe52838c0455022b44e76bb4
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2020-04-29 19:54:06 +0100

    Update calulate.js

--- calulate.js
+++ calulate.js
@@ -1,49 +1,41 @@
 
-        function calculate(){
-        var input = document.getElementById("input").value;
+function calculate(){
+var input = document.getElementById("input").value;
 var splitmul= "*"; var splitdiv= "/"; var splitadd= "+"; var splitsub= "-";
-        //division
         
-        if(input.includes(splitdiv)){
-  var div= input.split("/").map(x => +x);//changes the array of string created by split() function to an array of intergers 
+//division
+        
+if(input.includes(splitdiv)){
+var div= input.split("/").map(x => +x);//changes the array of string created by split() function to an array of intergers 
 var ans= div[0] / div[1];
 document.getElementById("ans").innerHTML= ans;
+ }
 
-//alert(typeof(ans)); 
-        
-        }
 //addition 
 
-       else if(input.includes(splitadd)){
- var div= input.split("+").map(x => +x);//changes the array of string created by split() function to an array of intergers
-     var ans= div[0] + div[1];
+else if(input.includes(splitadd)){
+var div= input.split("+").map(x => +x);//changes the array of string created by split() function to an array of intergers
+var ans= div[0] + div[1];
 document.getElementById("ans").innerHTML= ans;
+}
 
-        }
 //subtraction
 
-
-       else if(input.includes(splitsub)){
-        var div= input.split("-").map(x => +x);//changes the array of string created by split() function to an array of intergers
-     var ans= div[0] - div[1];
+else if(input.includes(splitsub)){
+var div= input.split("-").map(x => +x);//changes the array of string created by split() function to an array of intergers
+var ans= div[0] - div[1];
 document.getElementById("ans").innerHTML= ans;
-
 }
 
 //multiplication
 
-        else if(input.includes(splitmul)){
-  var div= input.split("*").map(x => +x);//changes the array of string created by split() function to an array of intergers   
+else if(input.includes(splitmul)){
+var div= input.split("*").map(x => +x);//changes the array of string created by split() function to an array of intergers   
 var ans= div[0] * div[1];
 document.getElementById("ans").innerHTML= ans;
-
 }
 
 else{
 alert("math error");
 }
-
-
 }
-        
-
